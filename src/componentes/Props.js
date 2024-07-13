850805260d5348a916f969fe26b85b8da532667a
Giuliano Genovesio <giulianogenovesio7@gmail.com>
2024-07-13 16:22:31 -0300

    Continue la clase 24

--- src/componentes/Props.js
+++ src/componentes/Props.js
@@ -4,7 +4,37 @@ const EjemploProps1 = props => {
     )
 }
 
+const EjemploProps2 = props =>{
+const { elementos } = props;
+
+return(
+<ul>{elementos.map(elemento =><li key={elemento}> {elemento} </li>)}</ul>
+)
+}
+
+
+const EjemploProps3 = ({titulo, subtitulo, cuerpo}) =>{
+return(
+    <div className="noticias">
+        <h4>{titulo}</h4>
+        <h5>{subtitulo}</h5>
+        <p>{cuerpo}</p>
+    </div>
+)
+}
+
+const EjemploProps4 = props =>{
+    return(
+<label>ver en consola
+    <input type="text" onChange={(e => props.cambiarvalor(e.target.value))} />
+</label>
+    )
+}
+
 export{
-EjemploProps1
+EjemploProps1,
+EjemploProps2,
+EjemploProps3,
+EjemploProps4
 // aca usamos {} y no export default porque son varios componentes distintos los que vamos a exportar
 }
--- src/pages/Ejemplo01.js
+++ src/pages/Ejemplo01.js
@@ -1,6 +1,37 @@
+import { EjemploProps1, EjemploProps2, EjemploProps3, EjemploProps4 } from "../componentes/Props";
+
+
+const nombre2 = 'Laura';
+const frutas = ['Manzana', 'Bananas', 'Palta', 'Naranjas'];
+
+const equipos =['Argentina', 'Colombia', 'Uruguay', 'Canada'];
+
+const mostrarValor = valor =>{
+    console.log(valor)
+}
+
+
+
 const Ejemplo01 = (props) => {
     return(
-       <p>hola spy la pagina de Ejemplo1</p>
+       <div>
+        <h1>Ejemplos de Propiedades</h1>
+        {/*propiedad simple y reutilizar*/}
+        <EjemploProps1 nombre='Giuliano' />
+        <EjemploProps1 nombre={nombre2} />
+        {/*listar elementos (array)*/}
+        <EjemploProps2 elementos={frutas} />
+        <EjemploProps2 elementos={equipos} />
+        {/*multiples propiedades - destructuring*/}
+        <EjemploProps3 titulo='Hola soy el titulo' subtitulo='subtitulo bien grande!' cuerpo='Hola el contenido de esta noticia.'/>
+        {/*funcion*/}
+        <EjemploProps4 cambiarvalor={mostrarValor} />
+        {/*este no anda porque si o si necesita cambiarvalor */}
+        <EjemploProps4 />
+
+        {/*como lo requiere si o si podemos tb pasarle una funcion vacia*/}
+        <EjemploProps4 cambiarvalor= {() =>{}} />
+       </div>
         
     )
 }
